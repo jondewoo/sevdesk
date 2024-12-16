54343a23c17a8e1b1cf3f38b6ec73f2559369523
Jonathan Derrough <jonathan.derrough@biojoule.de>
2024-12-16 12:20:08 +0100

    feat: Adds support for CommonJS using tsup

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "tsup";
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  entry: ["src/node.ts"],
+  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  // Generate declaration file (.d.ts)
+  dts: {
+    resolve: true,
+    compilerOptions: {
+      module: "NodeNext",
+      moduleResolution: "nodenext",
+    },
+  },
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+});
