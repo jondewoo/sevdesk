42137463913c7b7a3459d9ee58e858a045954960
Jonathan Derrough <jonathan.derrough@biojoule.de>
2025-03-05 17:04:58 +0100

    feat: Exports the types

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export { SevDeskClient } from "./client.js";
 export { SevDeskUrls } from "./urls.js";
 export type { SevDeskClientConfig } from "./client.js";
+export * as Types from "./interfaces.js";
